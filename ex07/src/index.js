6073a791eb1e9d9c9c2d16dc37ddf9c9bda5e777
SamraBA <90388771+SamraBA@users.noreply.github.com>
2021-10-01 23:42:11 +0200

    Update index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,11 +1,9 @@
-// Only change code below this line
-
-
 let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
 
-let uniqueArray = [...new Set(numberArray.sort())];
-console.log(uniqueArray.sort((function(a, b) { return a - b })));
+let uniqueArray = [...new Set(numberArray)].sort(function (a, b) {
+  return a - b;
+});
 
-// Only change code above this line
+console.log(uniqueArray);
 
 module.exports = { numberArray, uniqueArray };
